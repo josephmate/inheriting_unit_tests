3c0e2392eb016baa3bee3bcd5f40298c122c22e6
Joseph Mate <mate.joseph@gmail.com>
2016-08-27 21:42:04 -0400

    sketch of writeup done. few more edits and I'll put it on my blog

--- src/test/java/CompositionMapTest.java
+++ src/test/java/CompositionMapTest.java
@@ -25,7 +25,7 @@ public class CompositionMapTest {
 	/**
 	 * Getting a key that doesn't exist is null
 	 */
-	public void testNonExistantKey() {
+	private void testNonExistantKey() {
 		Map<Integer,Integer> map = mapFactory.make();
 		assertEquals(null, map.get(0));
 	}
@@ -33,7 +33,7 @@ public class CompositionMapTest {
 	/**
 	 * Getting a key that exist is returns the value
 	 */
-	public void testExistantReturnsCorrect() {
+	private void testExistantReturnsCorrect() {
 		Map<Integer,Integer> map = mapFactory.make();
 		map.put(0, 10);
 		assertEquals(Integer.valueOf(10), map.get(0));
@@ -42,7 +42,7 @@ public class CompositionMapTest {
 	/**
 	 * Getting a key that was replaced is returns the new value.
 	 */
-	public void testValueReplaced() {
+	private void testValueReplaced() {
 		Map<Integer,Integer> map = mapFactory.make();
 		map.put(0, 10);
 		map.put(0, 12);
